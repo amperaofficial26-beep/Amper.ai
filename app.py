8148b0b641f44cb17ef96d50db1e30cf5c49768a
Ampera_Official <amperaofficial26@gmail.com>
2026-08-08 11:34:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -911,14 +911,15 @@ with st.sidebar:
         st.rerun()
 
     st.markdown("---")
-'''
-    if auto_suggestions is not None and capcut_preset == "Normal / Manual":
-        if st.button("🪄 Auto Enhance Standar"):
-            for slider_key, val in auto_suggestions.items():
-                st.session_state[slider_key] = val
-            st.rerun()
-        st.markdown("---")
-'''
+
+
+    #if auto_suggestions is not None and capcut_preset == "Normal / Manual":
+    #    if st.button("🪄 Auto Enhance Standar"):
+    #        for slider_key, val in auto_suggestions.items():
+    #            st.session_state[slider_key] = val
+    #        st.rerun()
+    #    st.markdown("---")
+
     st.markdown("### 1. Light & Exposure")
     exposure = st.slider("Exposure", -2.0, 2.0, key="exposure", step=0.05)
     contrast = st.slider("Contrast", -50, 50, key="contrast", step=1)
